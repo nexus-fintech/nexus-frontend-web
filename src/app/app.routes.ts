3c3fec25b0b8ad43bbf84d14fc0a90a6265481ff
Angel Cancho <u201721995@upc.edu.pe>
2025-11-25 17:07:34 -0500

    feat: add new client and loan routes to application routing module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './public/pages/home/home.component';
+import { AboutComponent } from './public/pages/about/about.component';
+import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
+import { ClientManagementComponent } from './client/pages/client-management/client-management.component';
+import { LoanApplicationComponent } from './loan/pages/loan-application/loan-application.component';
+import { LoanEvaluationComponent } from './loan/pages/loan-evaluation/loan-evaluation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'clients', component: ClientManagementComponent, title: 'Nexus | Client Management' },
+  { path: 'loans/apply', component: LoanApplicationComponent, title: 'Nexus | Apply for Loan' },
+  { path: 'loans/evaluate', component: LoanEvaluationComponent, title: 'Nexus | Loan Evaluation & Risk' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
