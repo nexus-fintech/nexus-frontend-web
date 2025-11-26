98731df538501183b538fa4df810866db12c35ae
Angel Cancho <u201721995@upc.edu.pe>
2025-11-25 22:24:44 -0500

    feat(home): update layout and styles

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatCardModule, MatButton, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
