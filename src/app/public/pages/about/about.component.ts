d55952ea47d589521ded6ac8e1e63f5ab8021853
Angel Cancho <u201721995@upc.edu.pe>
2025-11-25 22:24:50 -0500

    feat(about): update layout and styles

--- src/app/public/pages/about/about.component.ts
+++ src/app/public/pages/about/about.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatListModule, MatIconModule, MatDividerModule, MatCardModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
