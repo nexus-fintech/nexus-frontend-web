abe5030fb4d6bc6674da009555d6a4996c2925fe
Angel Cancho <u201721995@upc.edu.pe>
2025-11-26 16:11:21 -0500

    feat(iam): integrate authentication interceptor and async animations into app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {authenticationInterceptor} from './iam/services/authentication.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient() ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient( withInterceptors([authenticationInterceptor]) ),
+    provideAnimationsAsync() ]
 };
